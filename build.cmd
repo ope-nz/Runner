9e64afb97aa596bbd3b3f9b4f5b358654ca77540
ope-nz <ope.nz.geeks@gmail.com>
2020-05-14 11:48:31 +1200

    Update

--- build.cmd
+++ build.cmd
@@ -1,5 +1,3 @@
-if not exist out (md out)
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe /target:winexe /out:Runner.exe /win32icon:favicon.ico runner.cs
 
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe /target:winexe /out:out\Runner.exe /win32icon:favicon.ico code\runner.cs
-REM c:\WINDOWS\Microsoft.NET\Framework\v3.5\csc /target:winexe /out:out\Runner.exe /win32icon:icon.ico code\Runner.cs
 pause

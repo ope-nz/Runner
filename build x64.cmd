42902ab8c0097bfdd2c7e8d5de2cf54713cb7048
ope-nz <ope.nz.geeks@gmail.com>
2021-02-01 20:24:43 +1300

    Update

--- build x64.cmd
+++ build x64.cmd
@@ -1,3 +1,3 @@
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe /target:winexe /out:Runner64.exe /win32icon:favicon.ico /platform:x64 runner.cs
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe /target:winexe /out:Runner64.exe /win32icon:favicon1.ico /platform:x64 runner.cs
 
 pause
